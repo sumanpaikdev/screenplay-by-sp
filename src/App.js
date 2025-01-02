3057ed5ad17cb9a6337c0b8d1aa7c7c0dedf2a1b
Suman Paik <reachout.suman@gmail.com>
2025-01-02 20:38:34 +0530

    final screenplay website added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+// default scroll from top of the page when revisit the page
+// import ScrollToTop from "./components/ScrollToTop";
+
+
+import HomePage from './pages/HomePage';
+import BlogDetails from './pages/BlogDetails';
+// import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <ScrollToTop /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+        {/* <Route path="/about" element={<AboutMe />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
